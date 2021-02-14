3e5b94e7304978dd2954e36431f28270da319020
c-4ke <glebkinkirill@gmail.com>
2021-02-14 14:38:08 +0000

    [LAB 1, TASK 4] Updated generate_random_numbers.sh. To launch average.sh with data from this script: ./average.sh  39725 53971 1264 14753 20583 62178 5694 30692 48242 62443 40993 48089 53632 37549 31182 12023 31637 36891 10495 11228 62153 28073 49065 63647 18835 15117 56080 9041 41905 23421 15264 39315 41416 24553 33130 44654 53834 14793 64693 58776 24947 18099 65512 19761 19792 36362 6373 24641 33701 45144 41764 21648 1905 62630 6406 58544 19135 35196 61153 27176 34258 30510 2259 42193 33580 6361 30511 40534 6399 10385 31909 58287 3149 14871 21344 65255 30750 55097 18255 36680 51598 18943 59771 20264 23916 18138 43227 48174 25117 60062 54361 58893 31681 46224 23405 57670 3182 14134 5630 43392 18086 54539 29982 10148 57856 38494 26607 14749 46358 35867 51600 8255 45918 58874 29626 47942 34137 8943 34486 12745 22165 26347 53594 16310 48014 53879 60926 15112 39511 42537 63153 54271 30765 23764 17281 34094 16430 64873 39426 54911 22445 34258 65490 53350 48371 29541 56103 40414 49821 20196 after ./generate_random_numbers.sh > numbers.txt

--- lab1/generate_random_numbers.sh
+++ lab1/generate_random_numbers.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 count=150
-while [ $count -ne 1 ]; do
-echo -n "$(od -An -N2 -i /dev/random)"
+temp=""
+while [ $count -ne 0 ]; do
+temp="$temp$(od -An -N2 -i /dev/random)"
 count=$(( count - 1 ))
 done
-echo "$(od -An -N2 -i /dev/random)"
+echo "$(echo "$temp" | tr -s " ")"
 exit 0 
