b17bba7db28eb40efe5519f8a26eab771ab92bf5
c-4ke <glebkinkirill@gmail.com>
2021-02-14 14:00:46 +0000

    [LAB 1, TASK 4] Updated generate_random_numbers.sh. Command for text document to use it for average.sh : cat numbers.txt | tr -s " "

--- lab1/generate_random_numbers.sh
+++ lab1/generate_random_numbers.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 count=150
-while [ $count -ne 0 ]; do
-od -An -N2 -i /dev/random
+while [ $count -ne 1 ]; do
+echo -n "$(od -An -N2 -i /dev/random)"
 count=$(( count - 1 ))
 done
+echo "$(od -An -N2 -i /dev/random)"
 exit 0 
